540243dcae3ec06ea7d059a1722d1f0b4e34426f
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-05-12 14:45:03 +0100

    Added thin.py to thin chain files.

---
+++ thin.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+import bz2
+import glob
+import numpy as np
+import os.path as op
+import plotutils.autocorr as ac
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--overwrite', default=False, help='overwrite existing tchain files')
+    parser.add_argument('--dir', default='.', help='containing directory')
+
+    args = parser.parse_args()
+
+    for chainfile in glob.glob(op.join(args.dir, '*', 'chain.npy.bz2')):
+        try:
+            inp = bz2.BZ2File(chainfile, 'r')
+            try:
+                chain = np.load(inp)
+            finally:
+                inp.close()
+
+            tchain = ac.emcee_thinned_chain(chain)
+
+            out = bz2.BZ2File(op.join(op.dirname(chainfile), 'tchain.npy.bz2'), 'w')
+            try:
+                np.save(out, tchain)
+            finally:
+                out.close()
+        except:
+            print 'Could not complete thinning for ', chainfile
+            continue
