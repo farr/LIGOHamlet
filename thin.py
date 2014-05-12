e28c0c1de6765826056a8dca7386e9e867a006b2
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-05-12 18:30:08 +0100

    Updated thin.py to use temporary file and rename.

--- thin.py
+++ thin.py
@@ -4,33 +4,42 @@ import argparse
 import bz2
 import glob
 import numpy as np
+import os
 import os.path as op
 import plotutils.autocorr as ac
+import sys
+import tempfile
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--dir', default='.', help='containing directory')
+    parser.add_argument('--globdir', default='*', help='glob pattern for search directories')
 
     args = parser.parse_args()
 
-    for chainfile in glob.glob(op.join(args.dir, '*', 'chain.npy.bz2')):
-        try:
-            inp = bz2.BZ2File(chainfile, 'r')
+    for chainfile in glob.glob(op.join(args.globdir, 'chain.npy.bz2')):
+        dir = op.dirname(chainfile)
+        tchainfile = op.join(dir, 'tchain.npy.bz2')
+        temptchainfile = op.join(dir, 'temp_tchain.npy.bz2')
+        
+        if not op.isfile(tchainfile) or op.getmtime(chainfile) > op.getmtime(tchainfile):
             try:
-                chain = np.load(inp)
-            finally:
-                inp.close()
+                inp = bz2.BZ2File(chainfile, 'r')
+                try:
+                    chain = np.load(inp)
+                finally:
+                    inp.close()
 
-            tchain = ac.emcee_thinned_chain(chain)
-
-            out = bz2.BZ2File(op.join(op.dirname(chainfile), 'tchain.npy.bz2'), 'w')
-            try:
-                np.save(out, tchain)
-            finally:
-                out.close()
-        except:
-            print 'Could not complete thinning for ', chainfile
-            continue
-
-        print 'Thinned ', chainfile
+                tchain = ac.emcee_thinned_chain(chain)
+                tempout = bz2.BZ2File(temptchainfile, 'w')
+                try:
+                    np.save(tempout, tchain)
+                    os.rename(temptchainfile, tchainfile)
+                finally:
+                    tempout.close()
+            except:
+                print 'Could not thin ', chainfile
+                continue
+            print 'Thinned ', chainfile
+        else:
+            print 'Skipped ', chainfile, ' due to previous thinning'
